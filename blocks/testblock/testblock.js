114d69c862216494a3abd7e93a897edbb9d1af6d
Andrew Cavanagh <cavanaghacea@gmail.com>
2023-01-26 09:44:47 -0500

    add default import script and test block

---
+++ blocks/testblock/testblock.js
@@ -0,0 +1,6 @@
+import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+
+export default function decorate(block) {
+    console.log('decorating testblock');
+    console.log(block);
+}
